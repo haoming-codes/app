59090b7e9903af8dae47ec0248da67d3e2b8107e
Haoming Li <61410145+haoming-codes@users.noreply.github.com>
2025-10-22 19:56:54 -0700

    Add configurable IPA phone distance utilities

--- src/bilingual_ipa/__init__.py
+++ src/bilingual_ipa/__init__.py
@@ -1,4 +1,21 @@
 """Public API for bilingual IPA conversion."""
 from .converter import LanguageSegmenter, text_to_ipa
+from .phone_distance import (
+    AVAILABLE_DISTANCE_METRICS,
+    AggregateStrategy,
+    PhoneDistanceCallable,
+    combine_distances,
+    compute_distances,
+    phone_distance,
+)
 
-__all__ = ["text_to_ipa", "LanguageSegmenter"]
+__all__ = [
+    "text_to_ipa",
+    "LanguageSegmenter",
+    "AVAILABLE_DISTANCE_METRICS",
+    "AggregateStrategy",
+    "PhoneDistanceCallable",
+    "combine_distances",
+    "compute_distances",
+    "phone_distance",
+]
